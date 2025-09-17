4ce95f2c19a8d7f1a697879bf29353e69b5c364c
Leon <143452196+leonbennazar@users.noreply.github.com>
2025-09-17 06:00:02 -0300

    integrada Api snk

--- src/lib/components/titans.svelte
+++ src/lib/components/titans.svelte
@@ -1,18 +1,28 @@
-<script>
+<script lang="ts">
   import { onMount } from 'svelte';
+  let data: any[] = [];  //en typescript: data va a ser un array de cualquier tipo
 
-  let data = null;
-  
   async function getTitans() {
-    const response = await fetch('https://api.attackontitanapi.com/titans/1');
-    data = await response.json();
-    console.log(data);
+    const req = await fetch('https://api.attackontitanapi.com/titans/');
+    const res = await req.json();
+    data = res.results;  //uso esto porque la request devuelve objetos
+    console.log(data); //si la consola devuelve entre {}, es objeto, entre [] es array
   }
 
   onMount(() => {
     getTitans();
   });
 </script>
-{#if data}
-  <h1>{data.name}</h1>
-{/if}
+
+<div class="titanes">
+{#each data as titan}
+  <h1>{titan.name} {titan.img}</h1>
+  <img src= '/titans/{titan.id}.png' alt={titan.name}>
+{/each}
+</div>
+
+<style>
+.titanes img{
+  width: 500px;
+}
+</style>
